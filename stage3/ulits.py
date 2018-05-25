91cda6e895f55f006d3dc7c2201aa9268be0baf0
yooooo7 <gongyeqi.y7@gmail.com>
2018-05-25 21:11:26 +1000

    change feature's name

--- stage3/ulits.py
+++ stage3/ulits.py
@@ -19,7 +19,7 @@ training = spark.read.csv(DATA_PATH + train_file, header = False, inferSchema = 
 
 # Configure an ML pipeline, which consists of three stages: assembler, pca, naive bayes
 columns = training.columns[1:]
-assembler = VectorAssembler(inputCols = columns, outputCol = "v_features")
+assembler = VectorAssembler(inputCols = columns, outputCol = "features")
 # pca = PCA(k = 50, inputCol = assembler.getOutputCol(), outputCol = 'features')
 nb = NaiveBayes(smoothing = 1.0, modelType = "multinomial")
 pipeline = Pipeline(stages = [assembler, nb])
