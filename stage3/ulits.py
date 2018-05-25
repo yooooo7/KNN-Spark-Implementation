1da22f6a463b0f598b19f664f63f4322cee97c45
yooooo7 <gongyeqi.y7@gmail.com>
2018-05-25 20:19:10 +1000

    debug: show training

--- stage3/ulits.py
+++ stage3/ulits.py
@@ -16,6 +16,8 @@ train_file = 'Train-label-28x28.csv'
 
 # prepare training file
 training = spark.read.csv(DATA_PATH + train_file, header = False, inferSchema = "true").withColumnRenamed("_c0", 'label')
+training = training.withColunm("features", training.columns[1:])
+training.show()
 
 # Configure an ML pipeline, which consists of three stages: assembler, pca, naive bayes
 assembler = VectorAssembler(inputCols = training.columns[1:], outputCol = "features")
@@ -28,8 +30,8 @@ model = pipeline.fit(training)
 # prepare test file
 test = spark.read.csv(DATA_PATH + test_file, header = False, inferSchema = "true").withColumnRenamed("_c0", 'label')
 
-# prediction = model.transform(test)
+prediction = model.transform(test)
 
 prediction.show(5)
 
-rf = RandomForestClassifier
+# rf = RandomForestClassifier
