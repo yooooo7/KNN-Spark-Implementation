042d1bf43747fa5f4b6388648b6c14e2e28c8a8d
yooooo7 <gongyeqi.y7@gmail.com>
2018-05-25 21:09:38 +1000

    remove pca

--- stage3/ulits.py
+++ stage3/ulits.py
@@ -20,9 +20,9 @@ training = spark.read.csv(DATA_PATH + train_file, header = False, inferSchema = 
 # Configure an ML pipeline, which consists of three stages: assembler, pca, naive bayes
 columns = training.columns[1:]
 assembler = VectorAssembler(inputCols = columns, outputCol = "v_features")
-pca = PCA(k = 50, inputCol = assembler.getOutputCol(), outputCol = 'features')
+# pca = PCA(k = 50, inputCol = assembler.getOutputCol(), outputCol = 'features')
 nb = NaiveBayes(smoothing = 1.0, modelType = "multinomial")
-pipeline = Pipeline(stages = [assembler, pca, nb])
+pipeline = Pipeline(stages = [assembler, nb])
 
 model = pipeline.fit(training)
 
