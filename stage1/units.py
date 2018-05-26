87cd985e93b47d78461b991e1dbc23dd6cc4f5f8
yooooo7 <gongyeqi.y7@gmail.com>
2018-05-26 19:32:31 +1000

    debug

--- stage1/units.py
+++ stage1/units.py
@@ -21,9 +21,9 @@ class ListParam(AccumulatorParam):
             acc1[i] += acc2[i]
         return acc1
 
-TP_counter = spark.sparkContext.accumulator([0 for i in range(LABEL_NUM)], ListParam())
-FP_counter = spark.sparkContext.accumulator([0 for i in range(LABEL_NUM)], ListParam())
-FN_counter = spark.sparkContext.accumulator([0 for i in range(LABEL_NUM)], ListParam())
+TP_counter = spark.sparkContext.accumulator([0] * LABEL_NUM, ListParam())
+FP_counter = spark.sparkContext.accumulator([0] * LABEL_NUM, ListParam())
+FN_counter = spark.sparkContext.accumulator([0] * LABEL_NUM, ListParam())
 
 def init_par():
     parse = argparse.ArgumentParser()
@@ -93,11 +93,17 @@ class KNN(object):
         global FP_counter
         global FN_counter
         prediction, label = record
+        m = [0] * LABEL_NUM
+        n1 = [0] * LABEL_NUM
+        n2 = [0] * LABEL_NUM
         if prediction == label:
-            TP_counter[prediction] += 1
+            m[prediction] = 1
+            TP_counter += m
         else:
-            FN_counter[label] += 1
-            FP_counter[prediction] += 1
+            n1[label] = 1
+            FN_counter += n1
+            n2[prediction] = 1
+            FP_counter += n2
 
     def predict(self, test_pca):
         self.result = test_pca.rdd.map(self.getNeighbours)
@@ -116,3 +122,46 @@ class KNN(object):
 
 def stop_context():
     spark.stop()
+
+def main():
+    DATA_PATH = "/share/MNIST/"
+    # DATA_PATH = ''
+    test_file = 'Test-label-28x28.csv'
+    train_file = 'Train-label-28x28.csv'
+
+    dimension, k, output_path = init_par()
+    pca = pca_m(dimension)
+
+    # pre process for test dataset
+    test_df = read_CSV(DATA_PATH, test_file)
+    test_vectors = vectorization(test_df)
+    pca.fit_test(test_vectors)
+    test_pca = pca.pca_process(test_vectors)
+
+    # pre process for train dataset
+    train_df = read_CSV(DATA_PATH, train_file)
+    train_vectors = vectorization(train_df)
+    train_pca = pca.pca_process(train_vectors)
+
+    # divide train data to features and labels
+    tr_pca, tr_label = divide_train(train_pca)
+
+    # KNN
+    knn_m = KNN(tr_pca, tr_label)
+    result = knn_m.predict(test_pca)
+    print(result.take(5))
+
+    knn_m.con_m()
+
+    # format result and output
+    def format(record):
+        pre, ori = record
+        return ("label: {}, prediction: {};".format(ori, pre))
+
+    # formatted_res = result.map(format)
+    # formatted_res.saveAsTextFile(output_path)
+
+    stop_context()
+
+if __name__ == "__main__":
+    main()
