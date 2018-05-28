6f2859e1fd4c37341657a6ef849ec16fb25308c1
yooooo7 <gongyeqi.y7@gmail.com>
2018-05-29 00:44:02 +1000

    v2

--- stage1/units.py
+++ stage1/units.py
@@ -24,14 +24,14 @@ TP_counter = spark.sparkContext.accumulator([0] * LABEL_NUM, ListParam())
 FP_counter = spark.sparkContext.accumulator([0] * LABEL_NUM, ListParam())
 FN_counter = spark.sparkContext.accumulator([0] * LABEL_NUM, ListParam())
 
-def init_par():
-    parse = argparse.ArgumentParser()
-    parse.add_argument("--dimension", help = "PCA dimension", default = 50)
-    parse.add_argument("--k", help = "k nearest", default = 5)
-    args = parse.parse_args()
-    dimension = int(args.dimension)
-    k = int(args.k)
-    return (dimension, k)
+parse = argparse.ArgumentParser()
+parse.add_argument("--dimension", help = "PCA dimension", default = 50)
+parse.add_argument("--k", help = "k nearest", default = 5)
+args = parse.parse_args()
+dimension = int(args.dimension)
+k = int(args.k)
+
+REPRETATION_NUM = 16
 
 def read_CSV(DATA_PATH, datafile):
     test_df = spark.read.csv(DATA_PATH + datafile, header = False, inferSchema = "true")
@@ -57,6 +57,7 @@ class pca_m(object):
         return pca_result
 
 def divide_train(train_pca):
+    train_pca = train_pca.repartition(1)
     tr_pca = np.array(train_pca.select(train_pca['pca']).collect())[:, 0, :]
     tr_label = np.array(train_pca.select(train_pca['label']).collect())
     return tr_pca, tr_label
@@ -64,13 +65,11 @@ def divide_train(train_pca):
 tr_data = []
 tr_l = []
 class KNN(object):
-    def __init__(self, tr_pca, tr_label, k):
+    def __init__(self, tr_pca, tr_label):
         global tr_data
         global tr_l
         tr_data = spark.sparkContext.broadcast(tr_pca)
         tr_l = spark.sparkContext.broadcast(tr_label)
-
-        self.k = k
 
     @staticmethod
     def getNeighbours(record):
@@ -80,7 +79,7 @@ class KNN(object):
         tr_label = tr_l.value
         dis = np.sqrt( np.sum( ((train - test_features) ** 2), axis = 1 ))[:, np.newaxis]
         com = np.concatenate((tr_label, dis), axis = 1)
-        sorted_com = sorted(com, key = lambda x: x[1])[:self.k]
+        sorted_com = sorted(com, key = lambda x: x[1])[:k]
         tk_label = [x[0] for x in sorted_com]
         counts = np.bincount(tk_label)
         prediction = np.argmax(counts).item()
@@ -128,7 +127,6 @@ def main():
     test_file = 'Test-label-28x28.csv'
     train_file = 'Train-label-28x28.csv'
 
-    dimension, k = init_par()
     pca = pca_m(dimension)
 
     # pre process for train dataset
@@ -138,7 +136,7 @@ def main():
     train_pca = pca.pca_process(train_vectors)
 
     # pre process for test dataset
-    test_df = read_CSV(DATA_PATH, test_file)
+    test_df = read_CSV(DATA_PATH, test_file).repartition(16)
     test_vectors = vectorization(test_df)
     test_pca = pca.pca_process(test_vectors)
 
@@ -146,7 +144,7 @@ def main():
     tr_pca, tr_label = divide_train(train_pca)
 
     # KNN
-    knn_m = KNN(tr_pca, tr_label, k)
+    knn_m = KNN(tr_pca, tr_label)
     result = knn_m.predict(test_pca)
 
     # persist result after next action for future calculate
