70cbd340e2c3dd3df95780107b0c7e139782ec08
yooooo7 <gongyeqi.y7@gmail.com>
2018-05-29 16:41:02 +1000

    divide py file

--- stage1/utils.py
+++ stage1/utils.py
@@ -112,29 +112,3 @@ class KNN(object):
 
 def stop_context():
     spark.stop()
-
-def main():
-    train_pca = model.transform(train_df).select(['label', 'pca'])
-    test_pca =  model.transform(test_df).select(['label', 'pca']).repartition(REPRETATION_NUM)
-
-    # divide train data to features and labels
-    tr_pca, tr_label = divide_train(train_pca)
-
-    # KNN
-    knn_m = KNN(tr_pca, tr_label)
-    result = knn_m.predict(test_pca)
-
-    # persist result after next action for future calculate
-    result.persist()
-
-    # save result
-    result.collect()
-
-    # for each label, show precision recall and f1-score
-    knn_m.show_metrics()
-
-    # stop spark session instance
-    stop_context()
-
-if __name__ == "__main__":
-    main()
